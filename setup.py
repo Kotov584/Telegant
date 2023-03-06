bed8188d0a2c8b802a969cfd8e1c559ad527bced
Kotov584 <85634618+Kotov584@users.noreply.github.com>
2023-03-05 18:08:29 -0800

    AlphaRelease

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Telegant",
+    version="0.1.2023.03.05",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp"
+    ]
+)
