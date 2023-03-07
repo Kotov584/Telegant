daeb55eb14a10c88f2f8bd7ca49e6fae8436bebf
Kotov584 <85634618+Kotov584@users.noreply.github.com>
2023-03-07 04:23:35 -0700

    Update telegant.py

--- telegant/telegant.py
+++ telegant/telegant.py
@@ -111,14 +111,6 @@ class Telegant:
             except Exception as e:
                 print(f"Error answering callback query: {e}")
 
- 
-    async def answer_callback_query(self, callback_query_id):
-        async with aiohttp.ClientSession() as session:
-            try:
-                await session.post(f"{self.base_url}answerCallbackQuery", params={"callback_query_id": callback_query_id})
-            except Exception as e:
-                print(f"Error answering callback query: {e}")
-
     async def reply(self, chat_id, text, buttons=None):
         async with aiohttp.ClientSession() as session:
             params = {"chat_id": chat_id, "text": text}
